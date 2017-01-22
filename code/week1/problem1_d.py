c30695033f3873ab68073ee50d8d2e2aa78da8f6
Yapi Donatien Achou <ruben123x@gmail.com>
2017-01-22 16:52:03 +0100

    update

---
+++ code/week1/problem1_d.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+import matplotlib.pylab as plt
+import sys
+from scipy.integrate import odeint
+
+def eulers_method(h):
+    """
+    solve the system of ode:
+    
+    u''' = t*t*u*u'' - uv' 
+    v''  = tv*v' + 4*u'
+    u(0) = 1; u'(0) = 0; u''(0) = 0
+    v(0) = v'(0) = 1
+    
+    by Euler's method
+    Y[n+1] = Y[n] + h*F[n]
+    """
+    T = 1 # simulation time t in [0, T=1]
+    N = int(T/h) 
+    t = np.linspace(0, T, N+1)
+    x1 = np.zeros(N+1);x2 = np.zeros(N+1)
+    x3 = np.zeros(N+1);y1 = np.zeros(N+1)
+    y2 = np.zeros(N+1)
+    
+    #initial conditions
+    x1[0] = 1; x2[0] = 0; x3[0] = 0; y1[0] = 1; y2[0] = 1
+    for n in range(0, N):
+        x1[n+1] = x1[n] + h*x2[n]
+        x2[n+1] = x2[n] + h*x3[n]
+        x3[n+1] = x3[n] + h*(t[n]*t[n]*x1[n]*x3[n] - x1[n]*y2[n])
+        y1[n+1] = y1[n] + h*y2[n]
+        y2[n+1] = y2[n] + h*(t[n]*y1[n]*y2[n] + 4*x2[n])
+       
+    return x1, y1, t
+    
+
+
+def main():
+    """
+    call solver and plot solutions
+    """
+    
+    H = [ 0.25, 0.1, 0.001]
+    for h in H:
+        u, v, t = eulers_method(h)
+        N = int(1/h)
+        plt.subplot(211)
+        plt.plot(t,u)   
+        plt.title("u as a function of t for different values of h", color='blue')
+        plt.subplot(212)
+        plt.title("v as a function of t for different values of h", color='blue')
+        plt.plot(t,v)
+    plt.legend(['h={}'.format(H[0]),'h={}'.format(H[1]),'h={}'.format(H[2])] ,loc='best')    
+    plt.show()
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    #h = float(sys.argv[1])
+    main()
