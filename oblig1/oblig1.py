469178c8543978c3613759efd29954dc80750ab1
Yapi Donatien Achou <ruben123x@gmail.com>
2017-03-04 08:29:41 +0100

    update

--- oblig1/oblig1.py
+++ oblig1/oblig1.py
@@ -4,44 +4,28 @@ solver for oblig1 in mat260
 import numpy as np
 import matplotlib.pylab as plt
 from parameters import *
-
-def RKMethod(b,y,f,M,i,h,t):
+from math import sqrt
+def RKMethod(b,y,f,M,n,h,t):
     """
     Embeded Runge-Kutta method.
     """
     c1, c2, c3, c4, c5 = 0,1./3,1./3,1/2.,1.
-    for n in range(i, i+1):
-        for m in range(0, M):
-            k1 = f(t[n],y)
-            y1 = y + (1./3)*h*k1
-            k2 = f(t[n]+c1*h,y1)
-            y2 = y + (1./6)*h*k1 + (1./6)*h*k2
-            k3 = f(t[n]+c2*h,y2)
-            y3 = y + (1./8)*h*k1 + (0.)*h*k2 + (3./8)*h*k3
-            k4 = f(t[n]+c3*h,y3)
-            y4 = y + (1./2)*h*k1 + 0.*h*k2 + (-3./2)*h*k3 + 2*h*k4
-            k5 = f(t[n]+c4*h,y4)
-            y[m][n+1] = y[m][n]+h*(b[0]*k1[m][n] +\
-            b[1]*k2[m][n]+b[2]*k3[m][n]+b[3]*k4[m][n]+b[4]*k5[m][n])
-            #print "{}".format(i)
-    print " "
-    print "{}".format(i)
-    print y
+    for m in range(0, M):
+        k1 = f(t[n],y)
+        y1 = y + (1./3)*h*k1
+        k2 = f(t[n]+c1*h,y1)
+        y2 = y + (1./6)*h*k1 + (1./6)*h*k2
+        k3 = f(t[n]+c2*h,y2)
+        y3 = y + (1./8)*h*k1 + (0.)*h*k2 + (3./8)*h*k3
+        k4 = f(t[n]+c3*h,y3)
+        y4 = y + (1./2)*h*k1 + 0.*h*k2 + (-3./2)*h*k3 + 2*h*k4
+        k5 = f(t[n]+c4*h,y4)
+        y[m][n+1] = y[m][n]+h*(b[0]*k1[m][n] +\
+        b[1]*k2[m][n]+b[2]*k3[m][n]+b[3]*k4[m][n]+b[4]*k5[m][n])
     return y
      
-     
-def plot_solutions(method,b_higher,b_lower,y,f,t,N,M,h,T,c,tol,se,ie):
-    #e_h,i_h,r_h,N_h,e_v,i_v,N_v = RKstepSizeAlgorithm(method,b_higher,b_lower,T,c,tol,y,f,M,h,t)
-    s,i = RKstepSizeAlgorithm(method,b_higher,b_lower,y,f,t,N,M,h,T,c,tol,se,ie)
-    plt.plot(t,i,t,ie)
-    #plt.legend(['{} exact '.format(sol),'{} numerical'.format('0')], loc='best')
-    #plt.title('exact solution vs numerical solution for h = {}, T= {}'.format(h,T))
-    plt.ylim([-0.2,1.2])
-    #plt.savefig("latex_files/figures/%s.png"%fig, transparent = True)
-    plt.show()
-        
-            
-def RKstepSizeAlgorithm(method,b_higher,b_lower,y,f,t,N,M,h,T,c,tol,se,ie):
+                 
+def RKstepSizeAlgorithm(f,c,tol,y,h,M):
     """
     variable step size algorithm for Runge-Kutta method
     questions: whuch types of norm and how
@@ -49,34 +33,102 @@ def RKstepSizeAlgorithm(method,b_higher,b_lower,y,f,t,N,M,h,T,c,tol,se,ie):
     is N_v and N_h constants?
     what are the initial conditions?
     """ 
-    t0 = 0
-    p = 3;
-    N = int(T/h)
-    print " N ", N
-    for i in range(0,N):
-        Y = method(b_higher,y,f,M,i,h,t)
-        X = method(b_lower,y,f,M,i,h,t) 
+    
+    b_higher = [1/6.,0,0,2./3,1/6.]
+    b_lower = [1/2.,0,-3./2,2,0]
+    for n in range(0,N):
+        #h = t[n+1]-t[n]
+        Y = RKMethod(b_higher,y,f,M,n,h,t)
+        X = RKMethod(b_lower,y,f,M,n,h,t)
+       
+        #update
+        y, x = Y, X
+    error = Y-X
+    print " error ", error
+    return Y
+    
+    
+    
+def plot_solutions(solution,t):
+    u1,u2,u3,u4,u5,u6,u7 = solution
+    plt.plot(t,u1,t,u2)
+    #plt.legend(['{} exact '.format(sol),'{} numerical'.format('0')], loc='best')
+    #plt.title('exact solution vs numerical solution for h = {}, T= {}'.format(h,T))
+    plt.ylim([-0.2,1.2])
+    #plt.savefig("latex_files/figures/%s.png"%fig, transparent = True)
+    plt.show()
         
-        #K = max(error)
-        #print " error in for ", error
-        #while(K > (tol*h)/(T-t0) ): 
-            #h = c*h*( tol/((T-t0)*K) )**(1./(p+1))
-            #Y = method(b_higher,y,f,M,i,h,t)
-            #X = method(b_lower,y,f,M,i,h,t)
-            #error = np.sqrt(e1**2 + e2**2 + e3**2 + e4**2 + e5**2 + e6**2 + e7**2)
-            #K = max(error)  
-        #if K!=0:  
-            #h = h*( tol/((T-t0)*K) )**(1./(p+1))
-    return X
         
 if __name__ == "__main__":
-    h = 0.1
-    #y,f,t,N,M,h,T = MalariaModel()
-    y, f, se, ie, t, N, M, h,T = SIModel(h)
+    T = 100; 
+    
+    h = 0.01
+    N = int(T/h)
+    t = np.linspace(0,T,N+1)
+    
+    x1 = np.zeros(N+1);x2 = np.zeros(N+1)
+    x1[0] = 0.9; x2[0] = 0.1
+    y1 = np.array([x1,x2])
+    
+    
+    
+    e_h=np.zeros(N+1);i_h=np.zeros(N+1);r_h=np.zeros(N+1);N_h = np.zeros(N+1);e_v=np.zeros(N+1)
+    i_v=np.zeros(N+1);N_v = np.zeros(N+1)
+    N_h[0] = 400.+10.+30.+0 
+    N_v[0] = 1000.+100.+50.
+    e_h[0] = 10./(400+10+30+0)
+    i_h[0] = 10./(400+10+30+0)
+    r_h[0] = 0
+    e_v[0] = 100./(1000+100+50)
+    i_v[0] = 10./(1000+100+50)
+    y = np.array([e_h,i_h,r_h,N_h,e_v,i_v,N_v])
+    
+    print y
+    #f = MalariaModel()
+    def f1(t,y1):
+        beta = 0.5
+        f1 = -beta*y1[0]*y1[1]
+        f2 = beta*y1[0]*y1[1]
+        return np.array([f1,f2])
+        
+        
+    
+    def f2(t,y):
+        " function f"
+        #print " t = ",t
+        sigma_v = 0.6; sigma_h = 18.; beta_hv = 2.*1e-2; nu_h = 8.333*1e-2; phi_h = 7.666*1e-5
+        l_h = 3.285*1e-2; delta_h = 3.454*1e-4 ; gamma_h = 3.704*1e-3; rho_h = 1.460*1e-2
+        mu_1h = 0.1429;  mu_2h = 1e-7; beta_bar_vh =  8.333*1e-2 ; beta_vh = 2.*1e-2
+        nu_v = 0.1; phi_v = 0.4; mu_1v =0.1429 ;mu_2v = 1e-7
+        e_h = y[0];i_h = y[1];r_h = y[2];N_h = y[3];e_v = y[4];i_v = y[5];N_v = y[6]
+        #f2,f3,f4,f5,f6,f7
+        N = len(e_h)-1; f1 = np.zeros(N+1);f2 = np.zeros(N+1);f3 = np.zeros(N+1);f4 = np.zeros(N+1)
+        f5 = np.zeros(N+1);f6 = np.zeros(N+1);f7 = np.zeros(N+1)
+        
+        f1[t] = ((sigma_v*sigma_h*N_v[t]*beta_hv*i_v[t])/(sigma_v*N_v[t]+sigma_h*N_h[t]))*\
+             (1-e_h[t]-i_h[t]-r_h[t])-(nu_h+phi_h+(l_h/N_h[t]) )*e_h[t] + delta_h*i_h[t]*e_h[t]
+        f2[t] = nu_h*e_h[t]-(gamma_h+sigma_h+phi_h+(l_h/N_h[t]))*i_h[t]+sigma_h*i_h[t]*i_h[t]
+        f3[t] = gamma_h*i_h[t]-(rho_h+phi_h+(l_h/N_h[t]))*r_h[t]+sigma_h*i_h[t]*r_h[t]
+        f4[t] = l_h+phi_h*N_h[t]-(mu_1h+mu_2h*N_h[t])*N_h[t]-sigma_h*i_h[t]*N_h[t]
+        f5[t] =( (sigma_h*sigma_h*N_v[t])/(sigma_v*N_v[t]+sigma_h*N_h[t]) )*(beta_vh*i_h[t]+beta_bar_vh*r_h[t])-\
+               (nu_v+phi_v)*e_v[t]
+        f6[t] = nu_v*e_v[t]-phi_v*i_v[t]
+        f7[t] = phi_v*N_v[t]-(mu_1v+mu_2v*N_v[t])*N_v[t]
+        ft = np.array([f1,f2,f3,f4,f5,f6,f7])
+        #print "f7 :",f7
+        return ft
+    
+
+        
+    
+    M = 7 
     b_higher = [1/6.,0,0,2./3,1/6.]
     b_lower = [1/2.,0,-3./2,2,0]
     c,tol = 0.5, 0.01
-    plot_solutions(RKMethod,b_higher,b_lower,y,f,t,N,M,h,T,c,tol,se,ie)
+    #sol = RKstepSizeAlgorithm(f1,c,tol,y1,h,M)
+    
+    sol = RKstepSizeAlgorithm(f2,c,tol,y,h,M)
+    plot_solutions(sol,t)
     #plot_solutions(RKMethod,b_higher,b_lower,y,f,t,N,M,h,T,c,tol) 
               
              
--- oblig1/parameters.py
+++ oblig1/parameters.py
@@ -3,106 +3,47 @@ define parameters for a specific problem here
 example: set vectors y, f for y' = f, with all parameters
 """
 import numpy as np
-def SIModel(h):
-    """
-    setting parameters for SI
-    spreading of deseases model:
-    (s,i)' = (f1,f2) where 
-    f1 = -beta*s(t)*i(t)
-    f2 = beta*s(t)*i(t)
-    """
-    #vector solution
-    beta = 0.5
-    M=2; T = 20
-    N = int(T/h)
-    t = np.linspace(0,T, N+1)
-    s = np.zeros(N+1);i = np.zeros(N+1)
-    s[0] = 0.9; i[0] = 0.1 # x1 = s, x2 = i
-    y = np.array([s,i])
-    #setting function f = (f1,f2)
-    def f(t,y):
-        s,i = y[0],y[1]
-        beta = 0.5
-        f1 = -beta*s*i # f for s
-        f2 = beta*s*i # f for i
-        return np.array([f1,f2])
 
-    # setting analytical solutions
-    ie = 1./(1+9*np.exp(-beta*t))
-    se = 1-ie
-    return y, f, se, ie, t, N, M, h,T
-    
-    
     
 def MalariaModel():
     """
     set parameters for the malaria model
     """
-    M = 7
-    T = 300; h = 0.8
-    N = int(T/h)
-    t = np.linspace(0,T, N+1)
-    e_h,i_h,r_h,N_h,e_v,i_v,N_v = np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),\
-    np.zeros(N+1),np.zeros(N+1),np.zeros(N+1),np.zeros(N+1)
-    #initial conditions and model parameters
-    IC1 = {'S_h':400,'E_h':10,'I_h':30,'R_h':0, \
-                          'S_v':1000,'E_v':100,'I_v':50
-                         }
-    IC2 = {'S_h':700,'E_h':10,'I_h':30,'R_h':0, \
-                          'S_v':1000,'E_v':100,'I_v':50
-                         }
-    p = {'lamda_h':3.285*1e-2,'phi_h':7.666*1e-5,'phi_v':0.4, \
-             'beta_vh':0.8333,'beta_hv':2.*1e-2,'beta_bar_vh':8.333*1e-2,\
-             'sigma_h':18.,'sigma_v':0.6,'nu_h':8.333*1e-2, 'nu_v':0.1, \
-             'gamma_h':3.704*1e-3,'delta_h':3.454*1e-4,'rho_h':1.460*1e-2,\
-             'mu_1h':4.212*1e-5,'mu_1v':0.1429,'mu_2h':1e-7,'mu_2v':2.279*1e-4
-             }
-    
-    N_h[0] = IC1['S_h'] + IC1['E_h'] + IC1['I_h'] + IC1['R_h']
-    N_v[0] = IC1['S_v'] + IC1['E_v'] + IC1['I_v']
-    
-    e_h[0] = (IC1['E_h']/N_h[0])
-    i_h[0] = (IC1['I_h']/N_h[0])
-    r_h[0] = (IC1['R_h']/N_h[0])
-    e_v[0] = (IC1['E_v']/N_v[0])
-    i_v[0] = (IC1['I_v']/N_v[0])
-    y = np.array([e_h,i_h,r_h,N_h,e_v,i_v,N_v])
 
     def f(t,y):
         " function f"
-        e_h = y[0]
-        i_h = y[1]
-        r_h = y[2]
-        N_h = y[3]
-        e_v = y[4]
-        i_v = y[5]
-        N_v = y[6]
+        #print " t = ",t
+        sigma_v = 0.6; sigma_h = 18.; beta_hv = 2.*1e-2; nu_h = 8.333*1e-2; phi_h = 7.666*1e-5
+        l_h = 3.285*1e-2; delta_h = 3.454*1e-4 ; gamma_h = 3.704*1e-3; rho_h = 1.460*1e-2
+        mu_1h = 0.1429;  mu_2h = 1e-7; beta_bar_vh =  8.333*1e-2 ; beta_vh = 2.*1e-2
+        nu_v = 0.1; phi_v = 0.4; mu_1v =0.1429 ;mu_2v = 1e-7
+        e_h = y[0];i_h = y[1];r_h = y[2];N_h = y[3];e_v = y[4];i_v = y[5];N_v = y[6]
+        #f2,f3,f4,f5,f6,f7
+        N = len(e_h)-1; f1 = np.zeros(N+1);f2 = np.zeros(N+1);f3 = np.zeros(N+1);f4 = np.zeros(N+1)
+        f5 = np.zeros(N+1);f6 = np.zeros(N+1);f7 = np.zeros(N+1)
         
-        f1 = ( (p['sigma_v']*p['sigma_h']*N_v*p['beta_hv']*i_v)/(p['sigma_v']*N_v+p['sigma_h']*N_h) )* \
-        (1-e_h-i_h-r_h)-(p['nu_h']+p['phi_h']+(p['lamda_h']/N_h )*e_h )+p['delta_h']*i_h*e_h
-        
-        f2 = p['nu_h']*e_h-(p['gamma_h']+p['sigma_h']+p['phi_h']+(p['lamda_h']/N_h))*\
-           i_h+p['sigma_h']*i_h*i_h
-           
-        f3 = p['gamma_h']*i_h-(p['rho_h']+p['phi_h']+(p['lamda_h']/N_h))*r_h+\
-             p['sigma_h']*i_h*r_h
-             
-        f4 = p['lamda_h']+p['phi_h']*N_v-(p['mu_1h']+p['mu_2h']*N_h)-p['sigma_h']*i_h*N_h
-        
-        f5 = ((p['sigma_v']*p['sigma_h']*N_h)/(p['sigma_v']*N_v+p['sigma_h']*N_h))*\
-             (p['beta_vh']*i_h+p['beta_bar_vh']*r_h)*(1-e_v-i_v)-(p['nu_v']+p['phi_v'])*e_v
-             
-        f6 = p['nu_v']*e_v-p['phi_v']*i_v
-        f7 = p['phi_v']*N_v-(p['mu_1v']+p['mu_2v']*N_v)*N_v
-        f = np.array([f1,f2,f3,f4,f5,f6,f7])
-        print f
-        return f
+        f1[t] = ((sigma_v*sigma_h*N_v[t]*beta_hv*i_v[t])/(sigma_v*N_v[t]+sigma_h*N_h[t]))*\
+             (1-e_h[t]-i_h[t]-r_h[t])-(nu_h+phi_h+(l_h/N_h[t]) )*e_h[t] + delta_h*i_h[t]*e_h[t]
+        f2[t] = nu_h*e_h[t]-(gamma_h+sigma_h+phi_h+(l_h/N_h[t]))*i_h[t]+sigma_h*i_h[t]*i_h[t]
+        f3[t] = gamma_h*i_h[t]-(rho_h+phi_h+(l_h/N_h[t]))*r_h[t]+sigma_h*i_h[t]*r_h[t]
+        f4[t] = l_h+phi_h*N_h[t]-(mu_1h+mu_2h*N_h[t])*N_h[t]-sigma_h*i_h[t]*N_h[t]
+        f5[t] =( (sigma_h*sigma_h*N_v[t])/(sigma_v*N_v[t]+sigma_h*N_h[t]) )*(beta_vh*i_h[t]+beta_bar_vh*r_h[t])-\
+               (nu_v+phi_v)*e_v[t]
+        f6[t] = nu_v*e_v[t]-phi_v*i_v[t]
+        f7[t] = phi_v*N_v[t]-(mu_1v+mu_2v*N_v[t])*N_v[t]
+        ft = np.array([f1,f2,f3,f4,f5,f6,f7])
+        print "f7 :",f7
+        return ft
     
     #f(0,y)
-    return y,f,t,N,M,h,T
+    return f
     
     
-    
+def SIModel(y):
+    beta = 0.5
+    f1 = -beta*y[0]*y[1]
+    f2 = beta*y[0]*y[1]
+    return np.array([f1,f2])   
     
     
 if __name__ == "__main__":
